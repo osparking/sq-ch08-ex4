376180748651e2931204bd55d6060d762300fe4b
osparking <jbpark03@gmail.com>
2023-09-23 09:11:24 +0900

    initial w/ 2 dependencies

---
+++ src/main/java/space/jbp/sqCh08Ex4/SqCh08Ex4Application.java
@@ -0,0 +1,13 @@
+package space.jbp.sqCh08Ex4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh08Ex4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh08Ex4Application.class, args);
+	}
+
+}
---
+++ src/test/java/space/jbp/sqCh08Ex4/SqCh08Ex4ApplicationTests.java
@@ -0,0 +1,13 @@
+package space.jbp.sqCh08Ex4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh08Ex4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
