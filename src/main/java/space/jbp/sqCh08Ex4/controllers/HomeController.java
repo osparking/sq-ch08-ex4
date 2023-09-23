0edada9deea6a2b28ce2b48ea5276cb390d9cc8e
osparking <jbpark03@gmail.com>
2023-09-23 15:47:59 +0900

    home action method uses 2 path variables

--- src/main/java/space/jbp/sqCh08Ex4/controllers/HomeController.java
+++ src/main/java/space/jbp/sqCh08Ex4/controllers/HomeController.java
@@ -2,15 +2,15 @@ package space.jbp.sqCh08Ex4.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-	@RequestMapping("/home")
+	@RequestMapping("/home/{username}/{color}")
 	public String home(Model home,
-			@RequestParam(required = false) String username,
-			@RequestParam(required = false) String color) {
+			@PathVariable String username,
+			@PathVariable String color) {
 		home.addAttribute("username", username);
 		home.addAttribute("color", color);
 		return "home.html";
